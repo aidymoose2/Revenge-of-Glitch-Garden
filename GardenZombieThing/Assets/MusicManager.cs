138a956738d3c58b01c42d8d6565ebd961e9e710
Aidy <adrienne@ustwo.com>
2017-07-26 00:01:59 +0100

    Music Player Tutorial Complete. Ended up having to use code by Koyhaku on tutorial as OnLevelWasLoaded is redundant. Focused on delegates - not 100% clear yet what code is doing.

--- GardenZombieThing/Assets/MusicManager.cs
+++ GardenZombieThing/Assets/MusicManager.cs
@@ -5,47 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour {
 
+	public delegate void SceneChange(int level);
+	public static event SceneChange OnSceneChange;
 	public AudioClip[] levelMusicChangeArray;
 	private AudioSource audioSource;
-	private int level;
 
-	// Use this for initialization
-	void Awake () {
-	//Makes sure object is generated
-		DontDestroyOnLoad (gameObject);
-		Debug.Log("Don't destroy on load: " +name);
-	}
-	void Start(){
-	//Generates audioSource private variable by searching for Ausio Source component alraedy present on the game object
+	void Start()//Level Manager Script
+		{
 		audioSource = GetComponent<AudioSource>();
-		Debug.Log(levelMusicChangeArray);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	//Detects which level is being loaded
-	private void OnEnable (){
-		AudioClip thisLevelMusic = levelMusicChangeArray[level];
-		//Checks newly loaded level (active scene) against order of levels in build index, returning integer)
-		level = SceneManager.GetActiveScene().buildIndex;
-		Debug.Log ("Build index level loaded: " + level);
-		SceneManager.sceneLoaded += OnSceneLoaded; //subscribe
-		Debug.Log ("Playing clip: " + levelMusicChangeArray[level]);
+		if (OnSceneChange !=null)
+			{
+			OnSceneChange(SceneManager.GetActiveScene().buildIndex);
+			}
 		}
-	}
 
-	//private void onDisable (){
-		//SceneManager.sceneLoaded -= OnSceneLoaded; //unsubscribe
-	//}
+	void Awake()//Music Manager Script
+		{
+		DontDestroyOnLoad(gameObject);
+		MusicManager.OnSceneChange += MusicChange;
+		}
 
-	private delegate OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
-	{
-		if(thisLevelMusic) {//if there is an audio clip attached to the [level] number in the array
-			audioSource.clip = thisLevelMusic;
-			audioSource.loop = true;
-			audioSource.Play();
-			Debug.Log("Playing level " + level + " audio");
-	}
+	void MusicChange (int level)//OnLevelWasLoaded 
+		{
+			AudioClip curSceneMusic = levelMusicChangeArray[level];
+			if (curSceneMusic) //if there is an audio clip attached to the [level] number in the array
+				{
+					audioSource.clip = curSceneMusic;
+					audioSource.loop = true;
+					audioSource.Play();
+					Debug.Log("Playing audio for level " + level + " in build");
+				}
+		}
 }
