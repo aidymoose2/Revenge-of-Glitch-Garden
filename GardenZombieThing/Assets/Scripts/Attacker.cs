1a49044842122a5ceb6ccad00fb156b287b31ebc
Aidy <adrienne@ustwo.com>
2017-09-17 21:43:45 +0100

    Added colliders to attackers and defenders; added animation events to lizard

--- GardenZombieThing/Assets/Scripts/Attacker.cs
+++ GardenZombieThing/Assets/Scripts/Attacker.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour {
 
-	[Range (0f,1.5f)] public float walkSpeed; 
+	[Range (0f,1.5f)] public float currentSpeed; 
 	// Use this for initialization
 	void Start () {
-		
+		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+		myRigidbody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D()
+	{
+		print ("Collided with" + name); 
+	}
+	public void SetSpeed(float speed)
+	{
+		currentSpeed = speed;
 	}
 }
